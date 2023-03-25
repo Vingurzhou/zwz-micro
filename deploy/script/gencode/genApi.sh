055c909d7782dd9afc671d62ae18b1b1d50f2449
vingurzhou <zwz0123460218@icloud.com>
2023-03-26 05:40:26 +0800

    init

--- deploy/script/gencode/genApi.sh
+++
@@ -1,4 +0,0 @@
-service=greet
-mkdir -p "../../../app/${service}/cmd/api/desc"
-cd ../../../app/${service}/cmd/api/desc
-goctl api go -api *.api -dir ../ --style=gozero --home="../../../deploy/goctl/1.3.4"
--- deploy/script/gencode/genRpc.sh
+++
@@ -1,5 +0,0 @@
-service=user
-mkdir -p "../../../app/${service}/cmd/rpc/pb"
-cd ../../../app/${service}/cmd/rpc/pb
-goctl rpc protoc *.proto --go_out=../ --go-grpc_out=../ --zrpc_out=../ --style=gozero
-sed -i "" 's/,omitempty//g' *.pb.go
